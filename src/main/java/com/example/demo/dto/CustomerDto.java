6a7c82295faa8f87f019d53dd29c54cb08130c83
Ahmed Aziz <ahmed83@me.com>
2019-10-10 00:17:33 +0200

    mapstruct_1_2

--- src/main/java/com/example/demo/dto/CustomerDto.java
+++ src/main/java/com/example/demo/dto/CustomerDto.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerDto {
+    private String customerId;
     private String firstName;
     private String lastName;
     private String address;
